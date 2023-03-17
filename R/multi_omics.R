2c4f8f8167d2ede3483f8b2233d0809dafa230f2
jessica-ewald <33487514+jessica-ewald@users.noreply.github.com>
2023-03-17 15:27:46 -0400

    Add plot for explained variance

--- R/multi_omics.R
+++ R/multi_omics.R
@@ -1344,13 +1344,32 @@ PlotDimredVarexp <- function(imgNm, dpi=72, format="png"){
   dpi<-as.numeric(dpi)
   imgNm <- paste(imgNm, "dpi", dpi, ".", format, sep="");
   
-  sel.nms <- names(mdata.all)
-  data.list <- list()
-  for(i in 1:length(sel.nms)){
-    dat = readRDS(sel.nms[i])
-    data.list[[i]] <- dat$data.proc
-  }
   reductionSet <- .get.rdt.set();
+
+  df <- reductionSet$var.exp;
+  df <- reshape2::melt(df)
+  colnames(df) <- c("Factor", "Dataset", "value")
+  min_r2 = 0
+  max_r2 = max(df$value)
+  
+  p1 <- ggplot(df, aes_string(y="Factor", x="Dataset")) + 
+    geom_tile(aes_string(fill="value"), color="black") +
+    labs(x="", y="", title="") +
+    scale_fill_gradientn(colors=c("gray97","darkblue"), guide="colorbar", limits=c(min_r2,max_r2)) +
+    guides(fill=guide_colorbar("Var. (%)")) +
+    theme(
+      axis.text.x = element_text(size=rel(1.0), color="black"),
+      axis.text.y = element_text(size=rel(1.1), color="black"),
+      axis.line = element_blank(),
+      axis.ticks =  element_blank(),
+      panel.background = element_blank(),
+      strip.background = element_blank(),
+      strip.text = element_text(size=rel(1.0))
+    )
+  
+  Cairo(file=imgNm, width=10, height=8, type=format, bg="white", unit="in", dpi=dpi);
+  print(p1)
+  dev.off();
 }
 
 PlotDimredFactors <- function(imgNm, dpi=72, format="png"){
