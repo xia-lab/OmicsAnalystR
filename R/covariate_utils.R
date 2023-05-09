503209abb3c29da24a66d27f679ab189eec445d9
Guangyan Zhou <guangyan.zhou@mail.mcgill.ca>
2023-05-09 14:15:16 -0400

    fixing cov

--- R/covariate_utils.R
+++ R/covariate_utils.R
@@ -231,6 +231,7 @@ CovariateScatter.Anal <- function(dataName,
   names(fstat) <- names(p.value) <- rownames(dataSet$data.proc);
   fdr.p <- rest[,"adj.P.Val"];
   inx.imp <- p.value <= thresh;
+  inx.imp <- ifelse(is.na(inx.imp), FALSE, inx.imp);
   sig.num <- length(which(inx.imp == TRUE))
   
   if(sig.num > 0){ 
